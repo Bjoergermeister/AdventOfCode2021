59d0b574d24790791474680d207c00a09d1f3511
Bjoergermeister <b.svensson@ostfalia.de>
2021-12-17 11:24:52 +0100

    Day 9 Puzzle 2

--- Day9/main.rs
+++ Day9/main.rs
@@ -1,7 +1,7 @@
 use std::fs::File;
 use std::io::{ BufRead, BufReader };
 
-fn puzzle1(grid: Vec<i32>, line_length: usize) -> i32 {
+fn puzzle1(grid: &Vec<i32>, line_length: usize, low_points: &mut Vec<usize>) -> i32 {
     let mut risk_level_sum = 0;
     for i in 0..grid.len() {
         let left = (i % line_length == 0) || (grid[i - 1] > grid[i]);
@@ -10,10 +10,57 @@ fn puzzle1(grid: Vec<i32>, line_length: usize) -> i32 {
         let down = (i >= grid.len() - line_length) || (grid[i + line_length] > grid[i]);
 
         if left && up && right && down {
+            low_points.push(i);
             risk_level_sum += grid[i] + 1;
         }
     }
     return risk_level_sum;
+}
+
+fn calculate_basin_size(grid: &Vec<i32>, line_length: usize, current_position: usize, current_basin: &mut Vec<usize>) -> i32 {
+    current_basin.push(current_position);
+    let mut basin_size = 1;
+
+    if current_position % line_length != 0 && !current_basin.contains(&(current_position - 1)) {
+        if grid[current_position] < grid[current_position - 1] && grid[current_position - 1] < 9 {
+            basin_size += calculate_basin_size(grid, line_length, current_position - 1, current_basin);
+        }
+    }
+
+    if current_position >= line_length && !current_basin.contains(&(current_position - line_length)) {
+        if grid[current_position] < grid[current_position - line_length] && grid[current_position - line_length] < 9 {
+            basin_size += calculate_basin_size(grid, line_length, current_position - line_length, current_basin);
+        }
+    }
+
+    if current_position % line_length != line_length - 1 && !current_basin.contains(&(current_position + 1)){
+        if grid[current_position] < grid[current_position + 1] && grid[current_position + 1] < 9 {
+            basin_size += calculate_basin_size(grid, line_length, current_position + 1, current_basin);
+        }
+    }
+
+    if current_position < grid.len() - line_length && !current_basin.contains(&(current_position + line_length)){
+        if grid[current_position] < grid[current_position + line_length] && grid[current_position + line_length] < 9 {
+            basin_size += calculate_basin_size(grid, line_length, current_position + line_length, current_basin);
+        }
+    }
+    return basin_size;
+}
+fn puzzle2(grid: &Vec<i32>, line_length: usize, low_points: &Vec<usize>) -> i32 {
+    let mut all_basins = Vec::new();
+    let mut basin_sizes = Vec::new();
+    for low_point in low_points {
+        let mut current_basin = Vec::new();
+        basin_sizes.push(calculate_basin_size(grid, line_length, *low_point, &mut current_basin));
+        for i in 0..current_basin.len() {
+            all_basins.push(current_basin[i]);
+        }
+    }
+
+    basin_sizes.sort();
+
+    let n = basin_sizes.len();
+    return basin_sizes[n - 1] * basin_sizes[n - 2] * basin_sizes[n - 3];
 }
 
 fn main(){
@@ -31,5 +78,7 @@ fn main(){
         grid_size = line.len();
     }
 
-    println!("Puzzle 1: {}", puzzle1(grid, grid_size));
+    let mut low_points = Vec::new();
+    println!("Puzzle 1: {}", puzzle1(&grid, grid_size, &mut low_points));
+    println!("Puzzle 2: {}", puzzle2(&grid, grid_size, &low_points));
 }
