cce5050e029aed9c97f57a98b91c5db02901eb50
Bjoergermeister <b.svensson@ostfalia.de>
2021-12-22 19:22:41 +0100

    Day 10 Puzzle 2

--- Day10/main.rs
+++ Day10/main.rs
@@ -14,8 +14,8 @@ fn get_inverse_for_bracket(bracket: char) -> char {
 fn get_score_for_bracket(bracket: char) -> i32 {
     match bracket {
         ')' => return 3,
-        '}' => return 1197,
         ']' => return 57,
+        '}' => return 1197,
         '>' => return 25137,
         _ => return 0, 
     }
@@ -25,26 +25,45 @@ fn is_open_bracket(bracket: char) -> bool {
     return bracket == '(' || bracket == '{' || bracket == '[' || bracket == '<';
 }
 
-fn puzzle1(lines: &Vec<String>) -> i32 
+fn puzzle1(lines: &Vec<String>) -> (i32, i64) 
 {
-    let mut stack: Vec<char> = Vec::new();
-    let mut score = 0; 
+    let mut corrupted_score = 0;
+    let mut incomplete_scores = Vec::new(); 
     for i in 0..lines.len(){
+        let mut stack: Vec<char> = Vec::new();
         let line_chars: Vec<char> = lines[i].chars().collect();
+        let mut line_is_corrupted: bool = false;
         for j in 0..line_chars.len() {
-            let bracket: char = line_chars[j];
+            let bracket = line_chars[j];
             if is_open_bracket(bracket) {
                 stack.push(bracket);
             }else{
                 let last_open_bracket = stack.pop().unwrap();
                 if get_inverse_for_bracket(last_open_bracket) != bracket {
-                    score += get_score_for_bracket(bracket);
+                    corrupted_score += get_score_for_bracket(bracket);
+                    line_is_corrupted = true;
                     break;
                 }
             }
         }
+
+        if line_is_corrupted == false {
+            let mut incomplete_score: i64 = 0;
+            for i in 0..stack.len() {
+                match get_inverse_for_bracket(stack[stack.len() - i - 1]){
+                    ')' => incomplete_score = incomplete_score * 5 + 1,
+                    ']' => incomplete_score = incomplete_score * 5 + 2,
+                    '}' => incomplete_score = incomplete_score * 5 + 3,
+                    '>' => incomplete_score = incomplete_score * 5 + 4,
+                    _ => incomplete_score += 0,
+                }
+            }
+            incomplete_scores.push(incomplete_score);
+        }
     }
-    return score;
+
+    incomplete_scores.sort();
+    return (corrupted_score, incomplete_scores[(incomplete_scores.len() -1) / 2]);
 }
 
 fn main()
@@ -57,5 +76,7 @@ fn main()
         lines.push(line.unwrap());
     }
 
-    println!("Puzzle 1: {}", puzzle1(&lines));
+    let (puzzle1, puzzle2) = puzzle1(&lines);
+    println!("Puzzle 1: {}", puzzle1);
+    println!("Puzzle 2: {}", puzzle2);
 }
